938a4829c068b4e1068e2b05c71ccde4b675a693
Niket Naidu <coder137@gmail.com>
2022-07-15 01:00:55 -0700

    Added all

--- aoc/2021/all/src/main.rs
+++ aoc/2021/all/src/main.rs
@@ -3,6 +3,7 @@ use day2;
 use day3;
 use day4;
 use day5;
+use day6;
 
 fn main() {
     // NOTE, Just update this vector
@@ -17,6 +18,8 @@ fn main() {
         day4::day4_part2_solution(),
         day5::day5_part1_solution(),
         day5::day5_part2_solution(),
+        day6::day6_part1_solution(),
+        day6::day6_part2_solution(),
     ];
 
     solutions.iter().enumerate().for_each(|(index, data)| {
