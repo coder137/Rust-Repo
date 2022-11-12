93f592ad0e78e37eb173309179e6f3915f12f166
Niket Naidu <coder137@gmail.com>
2022-11-11 18:15:23 -0800

    Update day12.rs

--- aoc/2021/src/day12.rs
+++ aoc/2021/src/day12.rs
@@ -16,10 +16,11 @@ impl Cave {
     }
 }
 
+#[derive(Debug)]
 struct Graph<'a> {
     path: Vec<Cave>,
     allowed_paths: &'a HashMap<String, HashSet<String>>,
-    allowed_revisit: u32,
+    revisit_times: u32,
 }
 
 impl<'a> Graph<'a> {
@@ -27,19 +28,19 @@ impl<'a> Graph<'a> {
         Self {
             path,
             allowed_paths,
-            allowed_revisit: 0,
+            revisit_times: 0,
         }
     }
 
     fn new_with_allowed_revisit(
         path: Vec<Cave>,
         allowed_paths: &'a HashMap<String, HashSet<String>>,
-        allowed_revisit: u32,
+        revisit_times: u32,
     ) -> Self {
         Self {
             path,
             allowed_paths,
-            allowed_revisit,
+            revisit_times,
         }
     }
 
@@ -53,6 +54,10 @@ impl<'a> Graph<'a> {
             .map(|p| p.name.clone())
             .collect::<Vec<String>>()
             .join(",")
+    }
+
+    fn get_last_cave_travelled(&self) -> &Cave {
+        self.path.last().unwrap()
     }
 
     /// Step 2: Create a next API (similar to simulation)
@@ -71,15 +76,15 @@ impl<'a> Graph<'a> {
 
     /// Iter 3
     /// ...
-    fn compute_next_paths(&mut self) -> Option<Vec<Vec<Cave>>> {
-        let mut cave_paths: Vec<Vec<Cave>> = Vec::new();
-
+    fn compute_next_paths(&self) -> Option<Vec<Vec<Cave>>> {
         let current_cave = self.path.last().unwrap();
 
         if current_cave.name == "end" {
             // NOTE, If current_cave == "end" then we return None
             None
         } else {
+            let mut cave_paths: Vec<Vec<Cave>> = Vec::new();
+
             // NOTE, We append the new cave location to the end of self.path
             let allowed_paths_from_current_cave =
                 self.allowed_paths.get(&current_cave.name).unwrap();
@@ -88,7 +93,7 @@ impl<'a> Graph<'a> {
             //     "Current: {:?}, Allowed: {:?} Revisit: {}",
             //     self.get_cave_name_path(),
             //     allowed_paths_from_current_cave,
-            //     self.allowed_revisit,
+            //     self.revisit_times,
             // );
 
             for cave_name in allowed_paths_from_current_cave {
@@ -109,6 +114,72 @@ impl<'a> Graph<'a> {
                     let mut new_cave_path = self.path.clone();
                     new_cave_path.push(cave_to_go);
                     cave_paths.push(new_cave_path);
+                }
+            }
+
+            Some(cave_paths)
+        }
+    }
+
+    fn compute_next_path_as_graph(&self) -> Option<Vec<Graph<'a>>> {
+        let last_cave_travelled = self.get_last_cave_travelled();
+        if last_cave_travelled.name == "end" {
+            // NOTE, If current_cave == "end" then we return None
+            None
+        } else {
+            let mut cave_paths: Vec<Graph<'a>> = Vec::new();
+
+            // NOTE, We append the new cave location to the end of self.path
+            let allowed_paths_from_last_cave =
+                self.allowed_paths.get(&last_cave_travelled.name).unwrap();
+
+            // println!(
+            //     "Current: {:?}, Allowed: {:?} Revisit: {}",
+            //     self.get_cave_name_path(),
+            //     allowed_paths_from_last_cave,
+            //     self.revisit_times,
+            // );
+
+            for cave_name in allowed_paths_from_last_cave {
+                let cave_to_go = Cave::new(cave_name.clone());
+
+                // * Can we visit the cave?
+                // Big Cave: Yes
+
+                // Small Cave + Revisit allowed + Not Visited: Yes
+                // Small Cave + Revisit allowed + Visited: Yes
+                // Small Cave + No Revisit allowed + Not Visited: Yes
+                // Small Cave + No Revisit allowed + Visited: No
+
+                let add_cave = if !cave_to_go.big
+                    && self.revisit_times == 0
+                    && self.already_visited(&cave_to_go)
+                {
+                    false
+                } else {
+                    true
+                };
+
+                if add_cave {
+                    // Small Cave + Already Visited + We have time to visit
+                    let revisit_times = if !cave_to_go.big
+                        && self.already_visited(&cave_to_go)
+                        && self.revisit_times > 0
+                    {
+                        self.revisit_times - 1
+                    } else {
+                        self.revisit_times
+                    };
+
+                    let mut new_cave_path = self.path.clone();
+                    new_cave_path.push(cave_to_go);
+
+                    let graph = Graph::new_with_allowed_revisit(
+                        new_cave_path,
+                        self.allowed_paths,
+                        revisit_times,
+                    );
+                    cave_paths.push(graph);
                 }
             }
 
@@ -155,7 +226,7 @@ fn day12_part1(allowed_paths: &HashMap<String, HashSet<String>>) -> u32 {
     queue.push(start_graph);
 
     while !queue.is_empty() {
-        let mut current_graph = queue.pop().unwrap();
+        let current_graph = queue.pop().unwrap();
         let paths = current_graph.compute_next_paths();
 
         if paths.is_none() {
@@ -179,6 +250,43 @@ fn day12_part1(allowed_paths: &HashMap<String, HashSet<String>>) -> u32 {
 pub fn day12_part1_solution(path: &PathBuf) -> String {
     let allowed_paths = parse_values_from_file(path);
     day12_part1(&allowed_paths).to_string()
+}
+
+fn day12_part2(allowed_paths: &HashMap<String, HashSet<String>>) -> u32 {
+    let mut final_traversals = Vec::new();
+    let mut full_traversal_counter = 0;
+
+    let mut queue = Vec::new();
+    let start_graph =
+        Graph::new_with_allowed_revisit(vec![Cave::new("start".to_string())], &allowed_paths, 1);
+    queue.push(start_graph);
+
+    while !queue.is_empty() {
+        let current_graph = queue.pop().unwrap();
+        let paths = current_graph.compute_next_path_as_graph();
+        // println!("Paths: {:?}", paths);
+
+        if paths.is_none() {
+            // We have reached the end
+            final_traversals.push(current_graph.get_cave_name_path());
+            full_traversal_counter += 1;
+        } else {
+            // We have not reached the end
+            for g in paths.unwrap() {
+                let new_graph =
+                    Graph::new_with_allowed_revisit(g.path, g.allowed_paths, g.revisit_times);
+                queue.push(new_graph);
+            }
+        }
+    }
+
+    // println!("Final Traversal: {:#?}", final_traversals);
+    full_traversal_counter
+}
+
+pub fn day12_part2_solution(path: &PathBuf) -> String {
+    let allowed_paths = parse_values_from_file(path);
+    day12_part2(&allowed_paths).to_string()
 }
 
 #[cfg(test)]
@@ -258,6 +366,21 @@ mod tests {
     #[test]
     fn test_day12_part1_solution() {
         let solution = day12_part1_solution(&PathBuf::new().join("inputs").join("day12_input.txt"));
+        println!("Solution: {}", solution);
+    }
+
+    #[test]
+    fn test_day12_part2_example1() {
+        let data = ["start-A", "start-b", "A-c", "A-b", "b-d", "A-end", "b-end"];
+
+        let allowed_paths = test_parse(&data);
+        println!("Allowed_Paths: {:#?}", allowed_paths);
+        assert_eq!(day12_part2(&allowed_paths), 36);
+    }
+
+    #[test]
+    fn test_day12_part2_solution() {
+        let solution = day12_part2_solution(&PathBuf::new().join("inputs").join("day12_input.txt"));
         println!("Solution: {}", solution);
     }
 }
