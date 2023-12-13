c20dacda49b447b49a599a85d835b28c0f771515
Niket Naidu <coder137@gmail.com>
2023-12-13 00:13:44 -0800

    AOC 2023: Day 1 (#22)

---
+++ aoc/2023/src/main.rs
@@ -0,0 +1,30 @@
+use std::path::PathBuf;
+
+mod day1;
+// mod day2;
+// mod day3;
+// mod day4;
+
+fn main() {
+    // NOTE, Just update this vector
+    let solutions = [
+        day1::day1_part1_solution,
+        day1::day1_part2_solution,
+        // day2::day2_part1_solution,
+        // day2::day2_part2_solution,
+        // day3::day3_part1_solution,
+        // day3::day3_part2_solution,
+        // day4::day4_part1_solution,
+        // day4::day4_part2_solution,
+    ];
+
+    solutions.iter().enumerate().for_each(|(index, soln_cb)| {
+        let day = index / 2 + 1;
+        let part = index % 2 + 1;
+        let input_file = PathBuf::new()
+            .join("inputs")
+            .join(format!("day{}_input.txt", day));
+        let solution = soln_cb(common::read_file(&input_file));
+        println!("Day {} \tPart {} \t-> Solution: {}", day, part, solution);
+    });
+}
