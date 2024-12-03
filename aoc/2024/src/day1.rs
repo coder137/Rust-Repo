31b40c573e67dc7422b16710d0929505d9fdac31
Niket Naidu <coder137@gmail.com>
2024-12-02 23:06:19 -0800

    2024 Day 1 Part 2 solution

--- aoc/2024/src/day1.rs
+++ aoc/2024/src/day1.rs
@@ -1,41 +1,63 @@
-fn parse_input(input: String) -> Vec<Vec<String>> {
+use std::collections::{hash_map::Entry, HashMap};
+
+fn parse_input(input: String) -> Vec<(u32, u32)> {
     input
         .trim()
         .split('\n')
         .map(|d| {
-            d.trim()
-                .split(" ")
-                .filter(|d2| !d2.is_empty())
-                .map(|d2| d2.to_owned())
-                .collect::<Vec<_>>()
+            let mut iter = d.trim().split(" ").filter(|d2| !d2.is_empty());
+            let d1 = iter.next().unwrap();
+            let d1 = d1.parse::<u32>().unwrap();
+            let d2 = iter.next().unwrap();
+            let d2 = d2.parse::<u32>().unwrap();
+            (d1, d2)
         })
         .collect::<Vec<_>>()
 }
 
 pub fn day1_part1_solution(input: String) -> String {
     let input = parse_input(input);
-    let (mut alist, mut blist): (Vec<_>, Vec<_>) = input
-        .into_iter()
-        .map(|d| {
-            let d1 = d[0].parse::<u32>().unwrap();
-            let d2 = d[1].parse::<u32>().unwrap();
-            (d1, d2)
-        })
-        .unzip();
+    let (mut alist, mut blist): (Vec<_>, Vec<_>) = input.into_iter().unzip();
     alist.sort();
     blist.sort();
 
     let ans = alist
         .into_iter()
         .zip(blist)
-        .map(|(a, b)| (a).abs_diff(b))
+        .map(|(a, b)| a.abs_diff(b))
         .sum::<u32>();
     ans.to_string()
 }
 
-// pub fn day1_part2_solution(input: String) -> String {
-//     todo!()
-// }
+pub fn day1_part2_solution(input: String) -> String {
+    let input = parse_input(input);
+    let (alist, blist): (Vec<_>, Vec<_>) = input.into_iter().unzip();
+
+    let mut bmap = HashMap::<u32, u32>::default();
+    for b in blist {
+        match bmap.entry(b) {
+            Entry::Occupied(mut oe) => {
+                let v = oe.get_mut();
+                *v += 1;
+            }
+            Entry::Vacant(ve) => {
+                ve.insert(1);
+            }
+        }
+    }
+    let ans = alist
+        .into_iter()
+        .map(|a| {
+            let value = match bmap.get(&a) {
+                Some(value) => *value,
+                None => 0,
+            };
+            value * a
+        })
+        .sum::<u32>();
+
+    ans.to_string()
+}
 
 #[cfg(test)]
 mod tests {
@@ -55,5 +77,11 @@ mod tests {
         let ans = day1_part1_solution(INPUT_STR.into());
         println!("Ans: {ans}");
         // assert_eq!(ans, "142");
+    }
+
+    #[test]
+    fn test_day1_part2() {
+        let ans = day1_part2_solution(INPUT_STR.into());
+        println!("Ans: {ans}");
     }
 }
