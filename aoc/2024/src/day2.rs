b928249c19c70dbdbf74525e3e17af417a4f34bc
Niket Naidu <coder137@gmail.com>
2024-12-03 01:18:27 -0800

    Added aoc 2024: day 2 part 1 solution

---
+++ aoc/2024/src/day2.rs
@@ -0,0 +1,62 @@
+fn parse_input(input: String) -> Vec<Vec<u32>> {
+    input
+        .trim()
+        .split('\n')
+        .map(|d| {
+            d.trim()
+                .split(" ")
+                .map(|d| d.parse::<u32>().unwrap())
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>()
+}
+
+pub fn is_safe(level: Vec<u32>) -> bool {
+    let compare_is_decreasing = level[0] > level[1];
+    level.windows(2).all(|d| {
+        let is_decreasing = d[0] > d[1];
+        let same = compare_is_decreasing == is_decreasing;
+        let diff = d[0].abs_diff(d[1]);
+        (1..=3).contains(&diff) && same
+    })
+}
+
+pub fn day2_part1_solution(input: String) -> String {
+    let input = parse_input(input);
+    let ans = input
+        .into_iter()
+        .map(|i| if is_safe(i) { 1 } else { 0 })
+        .sum::<u32>();
+    ans.to_string()
+}
+
+// pub fn day2_part2_solution(input: String) -> String {
+//     todo!()
+// }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT_STR: &str = "7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9";
+
+    #[test]
+    fn test_day1_part1() {
+        let parsed = parse_input(INPUT_STR.into());
+        println!("parsed: {:?}", parsed);
+        let ans = day2_part1_solution(INPUT_STR.into());
+        println!("Ans: {ans}");
+        assert_eq!(ans, "2");
+    }
+
+    #[test]
+    fn test_day1_part2() {
+        // let ans = day2_part2_solution(INPUT_STR.into());
+        // println!("Ans: {ans}");
+    }
+}
