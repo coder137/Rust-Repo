1316699bd6924bdd248d6992f57b0bdffdf58e41
Niket Naidu <coder137@gmail.com>
2024-12-03 22:56:49 -0800

    Added AOC 2024: Day 3 Part 2

--- aoc/2024/src/day3.rs
+++ aoc/2024/src/day3.rs
@@ -1,17 +1,3 @@
-// fn parse_input(input: String) -> Vec<Vec<u32>> {
-//     input
-//         .trim()
-//         .split('\n')
-//         .map(|d| {
-//             d.trim()
-//                 .split(" ")
-//                 .map(|d| d.parse::<u32>().unwrap())
-//                 .collect::<Vec<_>>()
-//         })
-//         .collect::<Vec<_>>()
-// }
-
-// mul(x,y)
 pub fn day3_part1_solution(input: String) -> String {
     let re = regex::Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
     let ans = re
@@ -29,11 +15,35 @@ pub fn day3_part1_solution(input: String) -> String {
     ans.to_string()
 }
 
-// pub fn day3_part2_solution(input: String) -> String {
-//     let re = regex::Regex::new(r"(mul\((\d{1,3}),(\d{1,3})\)|don't\(\)|do\(\))").unwrap();
-//     let ans = re.captures_iter(&input).map(|d| {});
-//     todo!()
-// }
+pub fn day3_part2_solution(input: String) -> String {
+    let re = regex::Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)|don't\(\)|do\(\)").unwrap();
+
+    let mut ans = 0;
+    let mut process = true;
+    for capture in re.captures_iter(&input) {
+        let identifier = capture.get(0).unwrap().as_str();
+        if identifier.contains("mul") {
+            if !process {
+                continue;
+            }
+            let a_str = capture.get(1).unwrap().as_str();
+            let b_str = capture.get(2).unwrap().as_str();
+            let multiplied = if (1..=3).contains(&a_str.len()) && (1..=3).contains(&b_str.len()) {
+                a_str.parse::<u32>().unwrap() * b_str.parse::<u32>().unwrap()
+            } else {
+                0
+            };
+            ans += multiplied;
+        } else if identifier.contains("don't") {
+            process = false;
+        } else if identifier.contains("do") {
+            process = true;
+        } else {
+            unreachable!()
+        }
+    }
+    ans.to_string()
+}
 
 #[cfg(test)]
 mod tests {
@@ -42,19 +52,19 @@ mod tests {
     const INPUT_STR: &str =
         "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
 
+    const INPUT_STR2: &str =
+        "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
     #[test]
-    fn test_day1_part1() {
-        // let parsed = parse_input(INPUT_STR.into());
-        // println!("parsed: {:?}", parsed);
+    fn test_part1() {
         let ans = day3_part1_solution(INPUT_STR.into());
         println!("Ans: {ans}");
-        // assert_eq!(ans, "2");
+        assert_eq!(ans, "161");
     }
 
-    // #[test]
-    // fn test_day1_part2() {
-    //     let ans = day2_part2_solution(INPUT_STR.into());
-    //     println!("Ans: {ans}");
-    //     assert_eq!(ans, "4");
-    // }
+    #[test]
+    fn test_part2() {
+        let ans = day3_part2_solution(INPUT_STR2.into());
+        println!("Ans: {ans}");
+        assert_eq!(ans, "48");
+    }
 }
